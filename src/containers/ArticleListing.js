6d72efa99a55f7801791ab5c464780cd8dd52695
jokamanlee <jl10630@nyu.edu>
2020-09-15 15:29:53 -0400

    Add in data

--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,15 +1,20 @@
 import React from "react";
 //Component Imports
 import ArticleCard from "../components/ArticleCard";
+//Data Imports
+import Data from "../components/data";
 
 function ArticleListing() {
+  //props  = sends data through the flow of the components...
   return (
-    <section>
+    <main>
       <header>
         <h1>Articles</h1>
-        <ArticleCard />
       </header>
-    </section>
+      {Data.map((article, i) => (
+        <ArticleCard key={i} articleData={article} />
+      ))}
+    </main>
   );
 }
 
